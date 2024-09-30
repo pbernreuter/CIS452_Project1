050c18d45ead842ea01b8fa174a8c009f64ff8bc
pokey136 <pbernreu@gmail.com>
2024-09-30 11:44:46 -0400

    Progress I think

--- project1.c
+++ project1.c
@@ -7,6 +7,8 @@ void sigint_handler(int sigNum);
 
 int main() {
 
+    signal(SIGINT, sigint_handler);
+
 //~~~~~~~~~~~~~~~~~~~~~~~~ Number of Nodes from User~~~~~~~~~~~~~~~~~~~~~~~~
     int numberOfNodes;
     printf("How many nodes do you want to create?\n");
@@ -15,22 +17,76 @@ int main() {
         return 1;
     }
 
-//~~~~~~~~~~~~~~~~~~~~~~~~ Pipe Creation ~~~~~~~~~~~~~~~~~~~~~~~~
-    int pipes[numberOfNodes+1][2];
-    for (int i = 0; i < numberOfNodes+1; i++) {
-        if (pipe(pipes[i]) < 0) {
-            printf("Pipe creation failed\n");
-            return 1;
-        }
+//~~~~~~~~~~~~~~~~~~~~~~~~ Pipes for Parent ~~~~~~~~~~~~~~~~~~~~~~~~
+    int parentToFirstChild[2]; // Pipe for Parent to Child 0
+    int lastChildToParent[2];  // Pipe for Last Child to Parent
+    if (pipe(parentToFirstChild) == -1 || pipe(lastChildToParent) == -1) {
+        perror("pipe");
+        exit(1);
     }
+
+    int previousPipe[2];
 
 //~~~~~~~~~~~~~~~~~~~~~~~~ Node Creation ~~~~~~~~~~~~~~~~~~~~~~~~
     for (int i = 0; i < numberOfNodes; i++) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            printf("Child %d created\n", i);
-            break;
+
+        int nextPipe[2];
+        if (i < numberOfNodes - 1 && pipe(nextPipe) == -1) {
+            perror("pipe");
+            exit(1);
         }
+
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(1);
+        }
+
+        if (pid == 0) {
+            
+            char message[500];
+
+            if ( i == 0){ //First child
+                read(parentToFirstChild[0], message, sizeof(message));
+                printf("Child %d received message: %s\n", i, message);
+                write(nextPipe[1], message, sizeof(message));
+            } else if (i == numberOfNodes - 1) { //Last child
+                read(previousPipe[0], message, sizeof(message));
+                printf("Child %d received message: %s\n", i, message);
+                write(lastChildToParent[1], message, sizeof(message));
+            } else { //Middle children
+                read(previousPipe[0], message, sizeof(message));
+                printf("Child %d received message: %s\n", i, message);
+                write(nextPipe[1], message, sizeof(message));
+
+            }
+        } else {
+            // Parent process
+
+            if (i > 0) {
+                // After forking child i, close the pipe used by the previous child
+                close(previousPipe[0]); // Close read end for the previous child
+                close(previousPipe[1]); // Close write end for the previous child
+            }
+
+            // Update previousPipe to the newly created nextPipe for the next iteration
+            previousPipe[0] = nextPipe[0];
+            previousPipe[1] = nextPipe[1];
+        }
+    }
+    while (1) {
+        char message[100];
+
+        // Parent sends a message to the first child
+        printf("Enter a message to send to the ring: ");
+        scanf(" %[^\n]s", message);  // Read a string message from the user
+
+        write(parentToFirstChild[1], message, sizeof(message));
+        printf("Parent (Node 0) sent message to child 0\n");
+
+        // Parent waits to receive the message back from the last child
+        read(lastChildToParent[0], message, sizeof(message));
+        printf("Parent received message back: %s\n", message);
     }
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~ Signal Handling ~~~~~~~~~~~~~~~~~~~~~~~~
