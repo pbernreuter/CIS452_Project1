97230b01a653d15026da94175649d707e2965b93
jaketonimore <veltmanj@mail.gvsu.edu>
2024-10-05 18:38:18 -0400

    Fixed output issues, still not quite finished

--- project1.c
+++ project1.c
@@ -6,10 +6,9 @@
 void sigint_handler(int sigNum);
 
 int main() {
-
     signal(SIGINT, sigint_handler);
 
-//~~~~~~~~~~~~~~~~~~~~~~~~ Number of Nodes from User~~~~~~~~~~~~~~~~~~~~~~~~
+    //~~~~~~~~~~~~~~~~~~~~~~~~ Number of Nodes from User~~~~~~~~~~~~~~~~~~~~~~~~
     int numberOfNodes;
     printf("How many nodes do you want to create?\n");
     if (scanf("%d", &numberOfNodes) != 1 || numberOfNodes < 1) {
@@ -17,7 +16,7 @@ int main() {
         return 1;
     }
 
-//~~~~~~~~~~~~~~~~~~~~~~~~ Pipes for Parent ~~~~~~~~~~~~~~~~~~~~~~~~
+    //~~~~~~~~~~~~~~~~~~~~~~~~ Pipes for Parent ~~~~~~~~~~~~~~~~~~~~~~~~ 
     int parentToFirstChild[2]; // Pipe for Parent to Child 0
     int lastChildToParent[2];  // Pipe for Last Child to Parent
     if (pipe(parentToFirstChild) == -1 || pipe(lastChildToParent) == -1) {
@@ -27,9 +26,8 @@ int main() {
 
     int previousPipe[2];
 
-//~~~~~~~~~~~~~~~~~~~~~~~~ Node Creation ~~~~~~~~~~~~~~~~~~~~~~~~
+    //~~~~~~~~~~~~~~~~~~~~~~~~ Node Creation ~~~~~~~~~~~~~~~~~~~~~~~~ 
     for (int i = 0; i < numberOfNodes; i++) {
-
         int nextPipe[2];
         if (i < numberOfNodes - 1 && pipe(nextPipe) == -1) {
             perror("pipe");
@@ -43,26 +41,25 @@ int main() {
         }
 
         if (pid == 0) {
-            
+            // Child process
             char message[500];
 
-            if ( i == 0){ //First child
+            if (i == 0) { // First child
                 read(parentToFirstChild[0], message, sizeof(message));
                 printf("Child %d received message: %s\n", i, message);
-                write(nextPipe[1], message, sizeof(message));
-            } else if (i == numberOfNodes - 1) { //Last child
+                write(nextPipe[1], message, sizeof(message)); // Pass message to next child
+            } else if (i == numberOfNodes - 1) { // Last child
                 read(previousPipe[0], message, sizeof(message));
                 printf("Child %d received message: %s\n", i, message);
-                write(lastChildToParent[1], message, sizeof(message));
-            } else { //Middle children
+                write(lastChildToParent[1], message, sizeof(message)); // Send message back to parent
+            } else { // Middle children
                 read(previousPipe[0], message, sizeof(message));
                 printf("Child %d received message: %s\n", i, message);
-                write(nextPipe[1], message, sizeof(message));
-
+                write(nextPipe[1], message, sizeof(message)); // Pass message to next child
             }
+            exit(0); // Exit child process after handling
         } else {
             // Parent process
-
             if (i > 0) {
                 // After forking child i, close the pipe used by the previous child
                 close(previousPipe[0]); // Close read end for the previous child
@@ -70,27 +67,57 @@ int main() {
             }
 
             // Update previousPipe to the newly created nextPipe for the next iteration
-            previousPipe[0] = nextPipe[0];
-            previousPipe[1] = nextPipe[1];
+            if (i < numberOfNodes - 1) {
+                previousPipe[0] = nextPipe[0];
+                previousPipe[1] = nextPipe[1];
+            }
         }
     }
+
     while (1) {
         char message[100];
+        int targetNode;
 
-        // Parent sends a message to the first child
+        // Parent sends a message to the specified child
         printf("Enter a message to send to the ring: ");
         scanf(" %[^\n]s", message);  // Read a string message from the user
 
+        printf("Enter the target node (0 to %d): ", numberOfNodes - 1);
+        scanf("%d", &targetNode);
+
+        // Validate target node
+        if (targetNode < 0 || targetNode >= numberOfNodes) {
+            printf("Invalid target node. Please enter a number between 0 and %d.\n", numberOfNodes - 1);
+            continue; // Skip the rest of the loop
+        }
+
+        // Send message to the first child
         write(parentToFirstChild[1], message, sizeof(message));
         printf("Parent (Node 0) sent message to child 0\n");
 
         // Parent waits to receive the message back from the last child
-        read(lastChildToParent[0], message, sizeof(message));
-        printf("Parent received message back: %s\n", message);
+        char response[500];
+        int currentNode = 0;
+
+        // Process the messages from children until it reaches the target node
+        while (1) {
+            read(lastChildToParent[0], response, sizeof(response));
+            printf("Parent received message: %s from Child %d\n", response, currentNode);
+            
+            // Check if the current node is the target node
+            if (currentNode == targetNode) {
+                printf("Parent received final message back from target Child %d: %s\n", targetNode, response);
+                break; // Exit the loop after receiving the target node's response
+            }
+
+            // Move to the next child in the ring
+            currentNode = (currentNode + 1) % numberOfNodes;
+        }
     }
 }
-//~~~~~~~~~~~~~~~~~~~~~~~~ Signal Handling ~~~~~~~~~~~~~~~~~~~~~~~~
-    void sigint_handler(int sigNum) {
-    printf("Signal Recieved. Exiting...\n");
+
+//~~~~~~~~~~~~~~~~~~~~~~~~ Signal Handling ~~~~~~~~~~~~~~~~~~~~~~~~ 
+void sigint_handler(int sigNum) {
+    printf("Signal Received. Exiting...\n");
     exit(0);
 }
